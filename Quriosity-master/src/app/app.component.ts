2b03d13b9f2ad846ce824247bb2c412cf785c08f
Abhishek Agarwal <142389739+abhishekagarwal777@users.noreply.github.com>
2023-11-01 11:50:24 +0530

    Add files via upload

---
+++ Quriosity-master/src/app/app.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  content = `But during battle, people will be willing to pay handsomely for my help. One thing is clear after talking to him,
+  those who want to create evolutionary change cannot do so while riding the wave created by the society. Humans/
+  (His skills brings about death of many, he too ends up getting murdered by the opposing side just when he thought
+  he had it all) Timeless 3. H-Time: How rich you are is measured by how much you give, not how much you have. Charities.
+  A son going after money all his life. His dad told him but didn&#39;t believe. One day he died and the boy knew
+  his dad was useless for all. Next part: he gets contacted by the charity his dad donated to. Humans. Timeless.
+  Quote 4. H-Time: Who wants to be young again? Who have already been young already? Why is it that you want to revisit
+  those times? What advice do you have for those who are sailing their ships right now? (Part 1 in humans, part 2
+  in time. Quote in CP) 5. Edu-Time: What about baiting people to learn? Knowing a kid wants ice-cream, to have him
+  read a book while promising him the treat. Can you change the world and have more people be educated that way?
+  Or do you just end up creating a lot of children with tooth- decay? (Part 1 story student being taught. Part 2
+  in time where he uses marketing to lure customers for his products, in the end gets sued for cancer causing ingredients,
+  ends up getting lots killed for temporary happiness)You won’t remember all the movies you’ve seen in your last
+  hour, you won’t think about all the jobs you’ve done, you won’t think about all those medals or competitions you’ve
+  won. Or how much gas your car needs or how many loans you still have to pay. Your bills and bank account numbers
+  will fade away from your mind when you’re an inch from death. You will forget all your desire for enmity and greed.
+  You won’t be able to recall what books you’ve read. You will ask yourself one thing: What did I do? And in that
+  one second, very few have the courage to be true to themselves. Noble Prize story. Dynamite. 2. H-Time: My skills
+  are useless in times of peace, but they&#39;re definitely going to come in handy during times of war. When there&#39;s
+  peace, no one cares for me. But during battle, people will be willing to pay handsomely for my help. One thing
+  is clear after talking to him, those who want to create evolutionary change cannot do so while riding the wave
+  created by the society. Humans/ (His skills brings about death of many, he too ends up getting murdered by the
+  opposing side just when he thought he had it all) Timeless 3. H-Time: How rich you are is measured by how much
+  you give, not how much you have. Charities. A son going after money all his life. His dad told him but didn&#39;t
+  believe. One day he died and the boy knew his dad was useless for all. Next part: he gets contacted by the charity
+  his dad donated to. Humans. Timeless. Quote 4. H-Time: Who wants to be young again? Who have already been young
+  already? Why is it that you want to revisit those times? What advice do you have for those who are sailing their
+  ships right now? (Part 1 in humans, part 2 in time. Quote in CP) 5. Edu-Time: What about baiting people to learn?
+  Knowing a kid wants ice-cream, to have him read a book while promising him the treat. Can you change the world
+  and have more people be educated that way? Or do you just end up creating a lot of children with tooth- decay?
+  (Part 1 story student being taught. Part 2 in time where he uses marketing to lure customers for his products,
+  in the end gets sued for cancer causing ingredients, ends up getting lots killed for temporary happiness)You won’t
+  remember all the movies you’ve seen in your last hour, you won’t think about all the jobs you’ve done, you won’t
+  think about all those medals or competitions you’ve won. Or how much gas your car needs or how many loans you still
+  have to pay. Your bills and bank account numbers will fade away from your mind when you’re an inch from death.
+  You will forget all your desire for enmity and greed. You won’t be able to recall what books you’ve read. You will
+  ask yourself one thing: What did I do? And in that one second, very few have the courage to be true to themselves.
+  Noble Prize story. Dynamite. 2. H-Time: My skills are useless in times of peace, but they&#39;re definitely going
+  to come in handy during times of war. When there&#39;s peace, no one cares for me. But during battle, people will
+  be willing to pay handsomely for my help. One thing is clear after talking to him, those who want to create evolutionary
+  change cannot do so while riding the wave created by the society. Humans/ (His skills brings about death of many,
+  he too ends up getting murdered by the opposing side just when he thought he had it all) Timeless 3. H-Time: How
+  rich you are is measured by how much you give, not how much you have. Charities. A son going after money all his
+  life. His dad told him but didn&#39;t believe. One day he died and the boy knew his dad was useless for all. Next
+  part: he gets contacted by the charity his dad donated to. Humans. Timeless. Quote 4. H-Time: Who wants to be young
+  again? Who have already been young already? Why is it that you want to revisit those times? What advice do you
+  have for those who are sailing their ships right now? (Part 1 in humans, part 2 in time. Quote in CP) 5. Edu-Time:
+  What about baiting people to learn? Knowing a kid wants ice-cream, to have him read a book while promising him
+  the treat. Can you change the world and have more people be educated that way? Or do you just end up creating a
+  lot of children with tooth- decay? (Part 1 story student being taught. Part 2 in time where he uses marketing to
+  lure customers for his products, in the end gets sued for cancer causing ingredients, ends up getting lots killed
+  for temporary happiness)You won’t remember all the movies you’ve seen in your last hour, you won’t think about
+  all the jobs you’ve done, you won’t think about all those medals or competitions you’ve won. Or how much gas your
+  car needs or how many loans you still have to pay. Your bills and bank account numbers will fade away from your
+  mind when you’re an inch from death. You will forget all your desire for enmity and greed. You won’t be able to
+  recall what books you’ve read. You will ask yourself one thing: What did I do? And in that one second, very few
+  have the courage to be true to themselves. Noble Prize story. Dynamite. 2. H-Time: My skills are useless in times
+  of peace, but they&#39;re definitely going to come in handy during times of war. When there&#39;s peace, no one
+  cares for me. But during battle, people will be willing to pay handsomely for my help. One thing is clear after
+  talking to him, those who want to create evolutionary change cannot do so while riding the wave created by the
+  society. Humans/ (His skills brings about death of many, he too ends up getting murdered by the opposing side just
+  when he thought he had it all) Timeless 3. H-Time: How rich you are is measured by how much you give, not how much
+  you have. Charities. A son going after money all his life. His dad told him but didn&#39;t believe. One day he
+  died and the boy knew his dad was useless for all. Next part: he gets contacted by the charity his dad donated
+  to. Humans. Timeless. Quote 4. H-Time: Who wants to be young again? Who have already been young already? Why is
+  it that you want to revisit those times? What advice do you have for those who are sailing their ships right now?
+  (Part 1 in humans, part 2 in time. Quote in CP) 5. Edu-Time: What about baiting people to learn? Knowing a kid
+  wants ice-cream, to have him read a book while promising him the treat. Can you change the world and have more
+  people be educated that way? Or do you just end up creating a lot of children with tooth- decay? (Part 1 story
+  student being taught. Part 2 in time where he uses marketing to lure customers for his products, in the end gets
+  sued for cancer causing ingredients, ends up getting lots killed for temporary happiness`;
+}
